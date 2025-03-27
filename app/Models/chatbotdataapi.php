290b032c1536681c7170e37b49ae5f34538033e6
shreya <shreya@gmail.com>
2025-03-27 21:37:50 +0530

    call

--- app/Models/chatbotdataapi.php
+++ app/Models/chatbotdataapi.php
@@ -1,25 +1,3 @@
-<!-- <?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-
-class ChatbotDataapi extends Model
-{
-    use HasFactory;
-
-    protected $table = 'atjoin_chatbot_data'; // Your table name
-    protected $primaryKey = 'user_id';
-    public $incrementing = false; // Assuming user_id is manually assigned
-    protected $keyType = 'string'; // Allow alphanumeric user IDs
-    // protected $fillable = ['user_id', 'name', 'email', 'contact', 'callback_requested', 'session_level'];
-    protected $fillable = ['user_id', 'name', 'contact', 'email', 'userquery', 'session_level', 'visitor_type'];
-
-    public $timestamps = false; // Disable automatic timestamps
-} -->
-
-
 <?php
 
 namespace App\Models;
